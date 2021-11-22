ce83a8348dbe78e8fe6676c80c28014af2636bc7
Anna-Mogilevskaya <mogilevs.anna@gmail.com>
2021-11-22 03:06:57 +0200

    task 1, 2
    create js for task 1, 2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,54 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createGalletyItemsMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function createGalletyItemsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+      <div class="gallery__item">
+        <a class="gallery__link"
+            href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>
+    `;
+    })
+    .join('');
+}
+
+galleryContainer.addEventListener('click', onShowGalleryModal);
+
+function onShowGalleryModal(event) {
+  event.preventDefault();
+  
+   if (event.target.nodeName !== 'IMG') {
+    return;
+  };
+  
+  const galleryItemSrc = event.target.dataset.source;
+    const instance = basicLightbox.create(
+        `<img src="${galleryItemSrc}" width="800" height="600">`
+    );
+  instance.show();
+  
+  document.addEventListener("keydown", event => {
+    if (event.key === "Escape") {
+            instance.close();
+        };
+
+});
+  
+};
+
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,41 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createGalletyItemsMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function createGalletyItemsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+        return `
+     <li> 
+        <a class="gallery__item"
+            href="${original}">
+            <img class="gallery__image"
+            src="${preview}" 
+            alt="${description}" />
+        </a>
+     </li>
+    `;
+    })
+    .join('');
+}
+
+galleryContainer.addEventListener('click', onShowGalleryModal);
+
+function onShowGalleryModal(event) {
+    event.preventDefault();
+
+     if (event.target.nodeName !== 'IMG') {
+    return;
+    };
+    
+    const lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250, captionPosition: 'bottom'});
+
+}
+
+
+
